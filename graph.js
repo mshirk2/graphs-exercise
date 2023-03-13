fd0934826000784ce25c4bf2d7aedab8e8cfe1d1
Michelle Shirk <michelle.l.shirk@gmail.com>
2023-03-13 15:07:14 -0400

    first commit

---
+++ graph.js
@@ -0,0 +1,84 @@
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  // this function accepts a Node instance and adds it to the nodes property on the graph
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
+
+  // this function accepts an array of Node instances and adds them to the nodes property on the graph
+  addVertices(vertexArray) {
+    for(let vertex of vertexArray){
+      this.addVertex(vertex);
+    }
+  }
+
+  // this function accepts two vertices and updates their adjacent values to include the other vertex
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
+
+  // this function accepts two vertices and updates their adjacent values to remove the other vertex
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
+
+  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+  removeVertex(vertex) {
+    for (let adjacent of vertex.adjacent){
+      this.removeEdge(adjacent, vertex);
+    }
+    this.nodes.delete(vertex);
+  }
+
+  // this function returns an array of Node values using DFS
+  depthFirstSearch(start) {
+    let values = [];
+    let toVisitStack = [start];
+    let seen = new Set(toVisitStack);
+
+    while (toVisitStack.length){
+      let currNode = toVisitStack.pop();
+      values.push(currNode.value);
+      for (let adjacent of currNode.adjacent){
+        if (!seen.has(adjacent)){
+          toVisitStack.push(adjacent);
+          seen.add(adjacent);
+        }
+      }
+    }
+    return values;
+  }
+
+  // this function returns an array of Node values using BFS
+  breadthFirstSearch(start) {
+    let values = [];
+    let toVisitQueue = [start];
+    let seen = new Set(toVisitQueue);
+
+    while (toVisitQueue.length){
+      let currNode = toVisitQueue.shift();
+      values.push(currNode.value);
+      for (let adjacent of currNode.adjacent){
+        if (!seen.has(adjacent)){
+          toVisitQueue.push(adjacent);
+          seen.add(adjacent);
+        }
+      }
+    }
+    return values;
+  }
+}
+
+module.exports = {Graph, Node}
